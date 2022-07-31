b6a06526f209dca0a7c070b2198c95c3e667eee3
Mulloa90 <mario90gm@icloud.com>
2022-07-31 16:14:13 -0700

    updated timer

--- assets/logic.js
+++ assets/logic.js
@@ -2,12 +2,14 @@ const startBtn = document.getElementById("start");
 const startScreen = document.getElementById("start-screen");
 const questionEl = document.getElementById("questions");
 const timerEl = document.getElementById("timer");
-
+const correctSound = new Audio("assets/sfx/correct.wav");
+const incorrectSound = new Audio("assets/sfx/incorrect.wav");
 startBtn.addEventListener("click", startGame);
 
 //Game variables
 let score = 0;
 let currentQuestion = 0;
+let secondsRemaining = 5;
 let timer;
 
 function startGame() {
@@ -17,8 +19,16 @@ function startGame() {
   renderQuestion();
   //show #questions
   questionEl.classList.remove("hide");
-  //start the timer and show the #timer
+  //Hydrate timerEl, show timerEl, start the timer
+  timerEl.textContent = `Time: ${secondsRemaining} seconds`;
   timerEl.classList.remove("hide");
+  timer = setInterval(function () {
+    secondsRemaining--;
+    timerEl.textContent = `Time: ${secondsRemaining} seconds`;
+    if (secondsRemaining <= 0) {
+      endGame();
+    }
+  }, 1000);
 }
 
 function renderQuestion() {
@@ -27,12 +37,32 @@ function renderQuestion() {
   const choices = questions[currentQuestion].choices;
   choices.forEach((choice) => {
     var button = document.createElement("button");
+    button.addEventListener("click", function () {
+      checkAnswer(choice);
+    });
     button.textContent = choice;
     document.getElementById("choices").append(button);
   });
 }
+function checkAnswer(choice) {
+  //evaluates whether answer is correct or incorrect
+  if (choice === questions[currentQuestion].answer) {
+    console.log("correct");
+    correctSound.play();
+  } else {
+    console.log("incorrect");
 
+    incorrectSound.play();
+  }
+
+  // sound effect and score
+  // sound effect and lose time
+
+  //transition to next question
+}
 function endGame() {
+    clearInterval(timer)
+    alert('game over')
   //stop the timer
   //hide #questions
   //show end-screen
